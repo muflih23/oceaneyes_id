7db02ac2b9e9098024b2226ae51803bc3f7043de
muhammad.muflih <muhammad.muflih@inteleq.id>
2025-01-11 12:29:08 +0700

    scroll to top on navigate

--- src/features/company-profile/fNavi/FNavi.jsx
+++ src/features/company-profile/fNavi/FNavi.jsx
@@ -1,9 +1,15 @@
+import { useEffect } from "react";
 import FNaviHeader from "./section/FNavi-header";
 import AppInfoSection from "./section/app-info";
 import FeatureSection from "./section/feature-section";
 import TrialInfoSection from "./section/trial-info";
 
 export default function FNaviPage() {
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
+
   return (
     <div className="flex flex-col w-full">
       <FNaviHeader />
